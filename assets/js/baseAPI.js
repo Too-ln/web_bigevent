286fdf6253d6018de33c4d13a64bec8728f295ac
nxl <2779948590@qq.com>
2022-09-04 20:38:43 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
 $.ajaxPrefilter(function(option){
-    options.url='http://www.liulongbin.top:3007'+options.url
+    option.url='http://www.liulongbin.top:3007'+option.url
+    if(option.url.indexOf('/my/')!==-1){
+        option.headers={Authorization:localStorage.getItem('token') || ''}
+    }
+
+    option.complete=function(res){
+        let {status,message}=res.responseJSON
+        console.log(status,message);
+        if(status===1  &&  message ==='身份认证失败！'){
+            localStorage.removeItem('token')
+            location.href='./login.html'
+        }
+    }
 })
+
